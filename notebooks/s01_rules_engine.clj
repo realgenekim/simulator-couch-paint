31fe836f65054e4fdc9a2229f873ddf336892ab0
Gene Kim <genek@itrevolution.com>
2023-02-09 15:36:19 -0800

    ::actor working; figured out how to bind vars

--- notebooks/s01_rules_engine.clj
+++ notebooks/s01_rules_engine.clj
@@ -16,10 +16,21 @@
      [:what
       [::this ::that 0]]
 
+     ::actor
+     [:what
+      [id ::role role]
+      [id ::name name]
+      [id ::x x]
+      [id ::y y]]
+
      ::supervisor
      [:what
       [::supervisor ::x x]
-      [::supervisor ::y y]]}))
+      [::supervisor ::y y]
+      [::supervisor :supervisor/name nm]
+      ,]}))
+
+(def *latest-id (atom 0))
 
 ; create session and add rule
 (def *session
@@ -36,4 +47,43 @@
       (o/insert ::supervisor ::y 30)
       (o/fire-rules))))
 
+(defn insert-entity!
+  [sess a v]
+  (let [id @*latest-id]
+    (swap! *latest-id inc)
+    (println :insert-entity id a v)
+
+    (swap! *session
+      (fn [session]
+        (-> session
+          (o/insert id
+                {::role :supervisor
+                 ::name v
+                 ::x 100
+                 ::y 100})
+          (o/fire-rules))))))
+
+
+
+(defn add-supervisors
+  " add steve and gene"
+  []
+  (insert-entity! *session nil "steve")
+  (insert-entity! *session nil "gene"))
+
+(add-supervisors)
+
+#_(swap! *session
+    (fn [session]
+      (-> session
+        (o/insert 1 ::role ::supervisor)
+        (o/insert 1 ::name "steve")
+        (o/insert 1 ::y 30)
+        (o/insert 1 ::x 50)
+        (o/fire-rules))))
+
+
+
 (o/query-all @*session)
+(o/query-all @*session ::supervisor)
+(o/query-all @*session ::actor)
