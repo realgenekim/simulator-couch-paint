c3ba8f171b3d6c3549ee76b5da981ffb235dcd7a
Gene Kim <genek@itrevolution.com>
2023-02-09 15:02:35 -0800

    first rule working

--- notebooks/s01_rules_engine.clj
+++ notebooks/s01_rules_engine.clj
@@ -1,4 +1,39 @@
 (ns s01-rules-engine
   (:require
     [nextjournal.clerk :as clerk]
-    [com.fulcrologic.guardrails.core :refer [>defn >defn- >def | ? =>]]))
+    [com.fulcrologic.guardrails.core :refer [>defn >defn- >def | ? =>]]
+    [odoyle.rules :as o]))
+
+
+; https://github.com/oakes/play-cljc-examples/blob/master/dungeon-crawler/src/dungeon_crawler/session.cljc
+
+
+; ## make first rules
+
+(def rules
+  (o/ruleset
+    {::steve
+     [:what
+      [::this ::that 0]]
+
+     ::supervisor
+     [:what
+      [::supervisor ::x x]
+      [::supervisor ::y y]]}))
+
+; create session and add rule
+(def *session
+  (atom (reduce o/add-rule (o/->session) rules)))
+
+;(o/insert *session ::steve ::supervisor true)
+;(o/insert *session ::supervisor ::x 20)
+;(o/insert *session ::supervisor ::y 30)
+
+(swap! *session
+  (fn [session]
+    (-> session
+      (o/insert ::supervisor ::x 20)
+      (o/insert ::supervisor ::y 30)
+      (o/fire-rules))))
+
+(o/query-all @*session)
