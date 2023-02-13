ed2081bafb3a843938871ee0662a9273bded05e5
Gene Kim <genek@itrevolution.com>
2023-02-12 17:30:04 -0800

    rooms, movers, painters

--- notebooks/s02_recursive_search.clj
+++ notebooks/s02_recursive_search.clj
@@ -2,4 +2,21 @@
   (:require
     [nextjournal.clerk :as clerk]
     [com.fulcrologic.guardrails.core :refer [>defn >defn- >def | ? =>]]
-    [genek.sim :as sim]))
+    [genek.sim2 :as sim]))
+
+;(remove-ns 's02-recursive-search)
+
+;; ## Rooms
+
+(sim/create-room 1)
+
+(clerk/table
+  sim/rooms)
+
+;; ## Movers
+
+(clerk/table sim/movers)
+
+;; ## Painters
+
+(clerk/table sim/painters)
--- src/genek/sim2.clj
+++ src/genek/sim2.clj
@@ -20,12 +20,32 @@
 (>defn create-room
   [id] [integer? => map?]
   {:id id
+   :role :room
    :state (first room-states)
    :moving1-time-remaining MOVING1-OP-TURNS
    :painting-time-remaining PAINTING-OP-TURNS
    :moving2-time-remaining MOVING2-OP-TURNS})
 
+(def rooms (for [r (range 10)]
+             (create-room r)))
 
+(>defn create-mover
+  [id] [integer? => map?]
+  {:id id
+   :role :mover
+   :at-room nil})
+
+(def movers (for [r (range 1)]
+              (create-mover r)))
+
+(>defn create-painter
+  [id] [integer? => map?]
+  {:id id
+   :role :painter
+   :at-room nil})
+
+(def painters (for [r (range 1)]
+                (create-painter r)))
 ;(defn insert-entity!
 ;  [sess a v]
 ;  (let [id @*latest-id]
