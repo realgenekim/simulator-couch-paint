0791c25d2a2872baf9865a0f10741afc335167d3
Gene Kim <genek@itrevolution.com>
2023-02-18 11:21:19 -0800

    rendering!

--- notebooks/s02_recursive_search.clj
+++ notebooks/s02_recursive_search.clj
@@ -8,6 +8,7 @@
 
 ;; ## State
 
+(sim/init-state!)
 (clerk/table @sim/*state)
 
 ;; assign mover to room
@@ -17,14 +18,19 @@
 
 (sim/next-turn! sim/*state newstate)
 
-(doseq [i (range 200)]
-  (let [newstate (-> (-> @sim/*state last)
-                   sim/assign-movers
-                   sim/free-movers
-                   sim/assign-painters
-                   sim/free-painters
-                   sim/advance-state)]
-    (sim/next-turn! sim/*state newstate)))
+(def newstates
+  (sim/simulate-until-done (-> @sim/*state last)))
+
+(reset! sim/*state newstates)
+
+#_(doseq [i (range 200)]
+    (let [newstate (-> (-> @sim/*state last)
+                     sim/assign-movers
+                     sim/free-movers
+                     sim/assign-painters
+                     sim/free-painters
+                     sim/advance-state)]
+      (sim/next-turn! sim/*state newstate)))
   ;(let [newstate (sim/assign-movers (-> @sim/*state last))]
   ;  (sim/next-turn! sim/*state newstate)))
 
