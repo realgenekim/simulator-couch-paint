41593527248527d742b395b0bbecbe86cf09a88b
Gene Kim <genek@itrevolution.com>
2023-02-27 15:52:51 -0800

    working svg > png!

--- notebooks/s05_from_adrian.clj
+++ notebooks/s05_from_adrian.clj
@@ -2,8 +2,14 @@
   (:require
     ;[membrane.skia :as skia]
     ;[membrane.component :refer [defui defeffect make-app]]
-    [membrane.ui :as ui]))
+    [membrane.ui :as ui])
     ;[ui.gk-membrane-ui :as gm]))
+  (:import
+    (com.kitfox.svg SVGUniverse)
+    (java.awt Graphics2D RenderingHints)
+    (java.awt.image BufferedImage)
+    (java.io File)
+    (javax.imageio ImageIO)))
 
 
 (import 'java.awt.image.BufferedImage
@@ -31,5 +37,25 @@
 (.drawString g "hello" 0 20)
 
 (skia/run (constantly
-            (ui/image
-               (get-image-bytes bi))))
+            (ui/vertical-layout
+              (ui/image
+                 (get-image-bytes bi))
+              (ui/image "furniture.png"))))
+
+(comment
+  (def svgu (SVGUniverse.))
+  (def svgd (.getDiagram svgu (.toURI (File. "furniture.svg"))))
+  ;(.loadSVG svgu (clojure.java.io/input-stream "furniture.svg"))
+
+  (def bi (BufferedImage. 1200 240 BufferedImage/TYPE_INT_ARGB))
+  ; https://stackoverflow.com/questions/46793769/bufferedimage-causes-a-program-freeze-on-macos-but-not-on-windows
+  (def g2d (.createGraphics bi))
+  ; ^^^ hangs
+  (type g2d)
+  (.setRenderingHint g2d RenderingHints/KEY_ANTIALIASING, RenderingHints/VALUE_ANTIALIAS_ON)
+
+  (type svgd)
+  (.render svgd g2d)
+  (ImageIO/write bi "PNG" (File. "furniture.png"))
+
+  0)
